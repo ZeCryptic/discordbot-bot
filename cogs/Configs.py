269521202eadbdaf6a4b426b02ef297cb537edec
ZeCryptic <29959674+zecryptic@users.noreply.github.com>
2021-04-24 20:38:50 +0200

    Made dev commands subcommands. Added command to delete log file of server. Made message logging also log emojies in message. Added support for standard emojies in "usage"

--- cogs/Configs.py
+++ cogs/Configs.py
@@ -14,43 +14,44 @@ class Configs(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.group()
     @commands.is_owner()
-    async def dev(self, ctx, arg):
+    async def dev(self, ctx):
         """Dev commands only accessible by the bot owners"""
-        command = arg.lower()
+        if ctx.invoked_subcommand is None:
+            await ctx.send('Invalid sub command')
 
-        # Updates the bot with the latest changes from github. Only works for updating cogs in runtime
-        if command == 'update':
-            await ctx.send('Pulling latest update from github...')
-            stream = os.popen('git pull')
-            output = stream.read()
-            await ctx.send(output)
+    @dev.command()
+    async def update(self, ctx):
+        await ctx.send('Pulling latest update from github...')
+        stream = os.popen('git pull')
+        output = stream.read()
+        await ctx.send(output)
 
-        # Restarts all cogs
-        elif command == 'reload':
-            embed = discord.Embed(
-                title='Reloading cogs',
-                timestamp=ctx.message.created_at
-            )
-            for cog in utils.get_cogs():
-                try:
-                    self.bot.unload_extension(cog)
-                except ExtensionNotLoaded:
-                    pass
-                try:
-                    self.bot.load_extension(cog)
-                    embed.add_field(
-                        name=f'Reloaded: {cog}',
-                        value='✅',
-                        inline=False)
-                except Exception as e:
-                    embed.add_field(
-                        name=f'Failed to reload: {cog}',
-                        value=f'❌: {e}',
-                        inline=False)
+    @dev.command()
+    async def reload(self, ctx):
+        embed = discord.Embed(
+            title='Reloading cogs',
+            timestamp=ctx.message.created_at
+        )
+        for cog in utils.get_cogs():
+            try:
+                self.bot.unload_extension(cog)
+            except ExtensionNotLoaded:
+                pass
+            try:
+                self.bot.load_extension(cog)
+                embed.add_field(
+                    name=f'Reloaded: {cog}',
+                    value='✅',
+                    inline=False)
+            except Exception as e:
+                embed.add_field(
+                    name=f'Failed to reload: {cog}',
+                    value=f'❌: {e}',
+                    inline=False)
 
-            await ctx.send(embed=embed)
+        await ctx.send(embed=embed)
 
     @dev.error
     async def dev_error(self, ctx, error):
@@ -59,6 +60,7 @@ class Configs(commands.Cog):
         else:
             await ctx.send('There was an error executing that command')
             print('Error')
+
 
 def setup(bot):
     bot.add_cog(Configs(bot))
